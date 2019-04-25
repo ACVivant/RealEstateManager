cfaa16c45f4415be01c5e00e50e100a104073765
ACVivant <annecharlotte.vivant@gmail.com>
2019-04-25 15:31:39 +0200

    bug correction

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.realestatemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
+    private TextView textViewQuantity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // CORRECTION 1
+        //this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main); erreur sur l'id fourni
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
+        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+
+        this.configureTextViewMain();
+        this.configureTextViewQuantity();
+    }
+
+    private void configureTextViewMain(){
+        this.textViewMain.setTextSize(15);
+        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+    }
+
+    private void configureTextViewQuantity(){
+        int quantity = Utils.convertDollarToEuro(100);
+        this.textViewQuantity.setTextSize(20);
+        // CORRECTION 2
+        //this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(String.valueOf(quantity));
+    }
+}
