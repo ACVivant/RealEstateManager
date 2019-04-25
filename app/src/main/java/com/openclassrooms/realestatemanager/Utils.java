0f29da011f36e7f0801a7f196d8103b0b570fb87
ACVivant <annecharlotte.vivant@gmail.com>
2019-04-25 15:41:10 +0200

    Utils corrections for OC

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -0,0 +1,70 @@
+package com.openclassrooms.realestatemanager;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+//import android.net.wifi.WifiManager;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
+
+/**
+ * Created by Philippe on 21/02/2018.
+ */
+
+public class Utils {
+
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param dollars
+     * @return
+     */
+    public static int convertDollarToEuro(int dollars){
+        return (int) Math.round(dollars * 0.812);
+    }
+
+    public static int convertEuroToDollar(int euros){
+        return (int) Math.round(euros / 0.812);
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @return
+     */
+//    public static String getTodayDate(){
+//        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd/MM");
+//        return dateFormat.format(new Date());
+//    }
+
+    public static String getTodayDate() {
+        return getFormatDate(new Date());
+    }
+
+    public static String getFormatDate(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return dateFormat.format(date);
+    }
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param context
+     * @return
+     */
+    public static Boolean isInternetAvailable(Context context){
+        //WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        //return wifi.isWifiEnabled();
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo !=null && networkInfo.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ app/src/test/java/com/openclassrooms/realestatemanager/UtilsTest.java
@@ -0,0 +1,38 @@
+package com.openclassrooms.realestatemanager;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Anne-Charlotte Vivant on 25/04/2019.
+ */
+
+public class UtilsTest {
+    Utils test = new Utils();
+
+    @Test
+    public void convertDollarToEuro_test() {
+        assertEquals(81.2, test.convertDollarToEuro(100), 0.5);
+    }
+
+    @Test
+    public void convertEuroToDollar_test() {
+        assertEquals(123, test.convertEuroToDollar(100), 0.5);
+    }
+
+        @Test
+    public void  getFormatDate_test() {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2019);
+        cal.set(Calendar.MONTH, 03); // Months are denombrated from 0 to 11
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        Date date = cal.getTime();
+
+        assertEquals("25/04/2019", test.getFormatDate(date));
+    }
+}
