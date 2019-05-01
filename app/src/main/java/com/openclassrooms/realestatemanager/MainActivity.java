b4512ee030d81384d5ceb50b8ec501c5a1594a13
ACVivant <annecharlotte.vivant@gmail.com>
2019-05-01 12:42:11 +0200

    first step of design for main activity

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,39 +1,137 @@
 package com.openclassrooms.realestatemanager;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-    private TextView textViewMain;
-    private TextView textViewQuantity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    //Design
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView  navigationView;
+
+    //Fragments
+    final MapHouseFragment fragment2 = new MapHouseFragment();
+    final ListHouseFragment fragment1 = new ListHouseFragment();
+
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
+
+    private int clic = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
+        this.configureBottomNavigationView();
 
-        // CORRECTION 1
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment1, "1").commit();
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // COnfigure toolbar
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
+    }
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void configureBottomNavigationView() {
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.bottom_navigation_list:
+                    fm.beginTransaction().hide(active).show(fragment1).commit();
+                    active = fragment1;
+                    getSupportActionBar().setTitle(R.string.nav_title);
+                    clic = 1; // To know which fragment was choosen how to design ActionBar
+                    invalidateOptionsMenu();
+                    return true;
+
+                case R.id.bottom_navigation_map:
+                    fm.beginTransaction().hide(active).show(fragment2).commit();
+                    active = fragment2;
+                    getSupportActionBar().setTitle(R.string.nav_title);
+                    clic = 2;
+                    invalidateOptionsMenu();
+                    return true;
+            }
+            return false;
+        }
+    };
+}
+
+
+
+//---------------------------------------------------------
+// A conserver
+//---------------------------------------------------------
+/*        // CORRECTION 1
         //this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main); erreur sur l'id fourni
         this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
-        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);*/
 
-        this.configureTextViewMain();
-        this.configureTextViewQuantity();
-    }
-
-    private void configureTextViewMain(){
-        this.textViewMain.setTextSize(15);
-        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
-    }
-
-    private void configureTextViewQuantity(){
-        int quantity = Utils.convertDollarToEuro(100);
-        this.textViewQuantity.setTextSize(20);
-        // CORRECTION 2
+/*        // CORRECTION 2
         //this.textViewQuantity.setText(quantity);
-        this.textViewQuantity.setText(String.valueOf(quantity));
-    }
-}
+        this.textViewQuantity.setText(String.valueOf(quantity));*/
+
--- app/src/main/java/com/openclassrooms/realestatemanager/WelcomeActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/WelcomeActivity.java
@@ -1,13 +1,45 @@
 package com.openclassrooms.realestatemanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import butterknife.BindView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+/*    @BindView(R.id.welcome_go_btn)
+    Button go_btn;*/
+
+    Button go_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        go_btn = findViewById(R.id.welcome_go_btn);
+        go_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*go_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
     }
+
 }
