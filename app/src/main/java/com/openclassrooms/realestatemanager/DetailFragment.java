cadcba6b104151269abd86f28cca9aa8850f9011
ACVivant <annecharlotte.vivant@gmail.com>
2019-05-09 19:01:34 +0200

    static map

--- app/src/main/java/com/openclassrooms/realestatemanager/DetailFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/DetailFragment.java
@@ -10,11 +10,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,6 +31,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -33,19 +40,29 @@ import java.util.Objects;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailFragment extends Fragment implements OnMapReadyCallback {
+public class DetailFragment extends Fragment {
+    //public class DetailFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String TAG = "DetailFragment";
     private static final float DEFAULT_ZOOM = 13f;
 
+    // Demo data
     private ArrayList<String> photoUrlList = new ArrayList<>();
     private ArrayList<String> textList = new ArrayList<>();
-    private ArrayList<Double> latitudeList = new ArrayList<>();
-    private ArrayList<Double> longitudeList = new ArrayList<>();
+    String number = "6";
+    String street = "Alexandre Dumas";
+    String zipcode = "60800";
+    String town = "Crépy-en-Valois";
+    String country = "France";
+
 
     private View v;
     private GoogleMap mMap;
     private Marker myHomeMarker;
+
+    /* @BindView(R.id.map-detail)
+ImageView mMapView;;*/
+ private ImageView mMapView;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -59,8 +76,16 @@ public class DetailFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         v= inflater.inflate(R.layout.fragment_detail, container, false);
 
+        mMapView = (ImageView) v.findViewById(R.id.map_detail);
+        //ButterKnife.bind(v); // Ca ne marche pas...
+
+
         initDemoData();
-        initMap();
+        initPhotosRecyclerView();
+
+        String key = getContext().getResources().getString(R.string.Google_Maps_API_Key);
+        initStaticMap(number, street,zipcode, town, country, key);
+        //initMap();
 
         return v;
     }
@@ -74,38 +99,25 @@ public class DetailFragment extends Fragment implements OnMapReadyCallback {
     private void initDemoData() {
         photoUrlList.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
         textList.add("Façade");
-        latitudeList.add(49.23);
-        longitudeList.add(2.92);
 
         photoUrlList.add("https://i.redd.it/tpsnoz5bzo501.jpg");
         textList.add("Cuisine");
-        latitudeList.add(49.2);
-        longitudeList.add(2.88);
 
         photoUrlList.add("https://i.redd.it/qn7f9oqu7o501.jpg");
         textList.add("Chambre 1");
-        latitudeList.add(49.28);
-        longitudeList.add(2.91);
 
         photoUrlList.add("https://i.redd.it/j6myfqglup501.jpg");
         textList.add("Chambre 2");
-        latitudeList.add(49.3);
-        longitudeList.add(2.9);
 
         photoUrlList.add("https://i.redd.it/0h2gm1ix6p501.jpg");
         textList.add("Salon");
-        latitudeList.add(49.23);
-        longitudeList.add(3.0);
 
         photoUrlList.add("https://i.redd.it/k98uzl68eh501.jpg");
         textList.add("Salle de bain");
-        latitudeList.add(49.35);
-        longitudeList.add(2.91);
 
-        initRecyclerView();
     }
 
-    private void initRecyclerView() {
+    private void initPhotosRecyclerView() {
         Log.d(TAG, "initRecyclerView");
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
@@ -119,33 +131,11 @@ public class DetailFragment extends Fragment implements OnMapReadyCallback {
     // Map
     //------------------------------------------------------------------
 
-    //initializing map
-    private void initMap() {
-        MapView mMapView;
-        mMapView = v.findViewById(R.id.map_detail);
-
-        if (mMapView != null) {
-            mMapView.onCreate(null);
-            mMapView.onResume();
-            mMapView.getMapAsync(this);
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitudeList.get(0), longitudeList.get(0)), DEFAULT_ZOOM));
-        addHomemarker();
-    }
-
-    private Marker addHomemarker() {
-        MarkerOptions options = new MarkerOptions()
-                .position(new LatLng(latitudeList.get(0), longitudeList.get(0)));
-
-        myHomeMarker = mMap.addMarker(options);
-        return myHomeMarker;
+    public void initStaticMap(String number, String street, String zipcode, String town, String country, String key) {
+        MapUrl mapUrl = new MapUrl();
+        String srcMap = mapUrl.createUrl(number, street, zipcode, town, country, key);
+        Log.d(TAG, "initStaticMap: " + srcMap);
+        Picasso.get().load(srcMap).into(mMapView);
     }
 
 }
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MapUrl.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.realestatemanager;
+
+import android.content.Context;
+
+import com.google.android.gms.common.ConnectionResult;
+
+/**
+ * Created by Anne-Charlotte Vivant on 09/05/2019.
+ */
+
+public class MapUrl {
+    private String base = "https://maps.googleapis.com/maps/api/staticmap?";
+    private String mapType = "&maptype=";
+    private String mapTypeInfos = "roadmap";
+    private String marker = "&markers=";
+    private String markerSize = "size:";
+    private String markerSizeInfos = "mid|";
+    private String markerColor = "color:";
+    private String markerColorInfos = "red|";
+    private String centerInfos;
+    private String size = "&size=";
+    private String sizeInfos = "300x300";
+    private String language = "&language=";
+    private String lanquageInfos = "french";
+    private String key = "&key=";
+    private String keyInfos;
+    Context mContext;
+
+public String formatAddressFieldsForMap(String input) {
+    String output= input.replace("-", "&");
+    output = output.replace(" ", "&");
+    return output;
+}
+
+public String extractSmallNameOfStreet(String input) { // Sinon GoogleMap est vraiment très fantaisiste
+    int index = input.lastIndexOf("&");
+    String output = input.substring(index+1);
+    return  output;
+}
+
+public void createAddressUrlField(String number, String street, String zipcode, String town, String country) {
+    String allAdress = number + "," + extractSmallNameOfStreet(formatAddressFieldsForMap(street)) + ',' +zipcode + ',' + formatAddressFieldsForMap(town) + ',' + formatAddressFieldsForMap(country);
+    centerInfos = allAdress;
+}
+
+public String createUrl(String number, String street, String zipcode, String town, String country, String myKey) {
+    keyInfos =  myKey;
+    createAddressUrlField(number, street, zipcode, town, country);
+    String url = base + size + sizeInfos + mapType + mapTypeInfos + marker + markerSize + markerSizeInfos + markerColor + markerColorInfos + centerInfos + language + lanquageInfos + key + keyInfos;
+    return url;
+}
+}
